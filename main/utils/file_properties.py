b7bc19efff280dd8cd8c9027d686b783bd4cad08
The_Wizard ඞ <103869920+Th3-Wizard@users.noreply.github.com>
2022-06-29 18:21:23 -0400

    Update file_properties.py

--- main/utils/file_properties.py
+++ main/utils/file_properties.py
@@ -83,12 +83,12 @@ async def gen_link(m: Message,log_msg: Messages, from_channel: bool):
     page_link = f"{Var.URL}watch/{get_hash(log_msg)}{log_msg.message_id}"
     stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     Stream_Text=lang.stream_msg_text.format(file_name, file_size, stream_link, page_link)
-    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥STREAM", url=page_link), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)]])
+    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💾Stream", url=page_link), InlineKeyboardButton("Download 📦", url=stream_link)]])
 
     if from_channel:
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥STREAM", url=page_link), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💾Stream", url=page_link), InlineKeyboardButton("Download 📦", url=stream_link)]])
     else:
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥STREAM", url=page_link), InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ 📥", url=stream_link)],
-            [InlineKeyboardButton("❌ Delete Link", callback_data=f"msgdelconf2_{log_msg.message_id}_{get_media_file_unique_id(log_msg)}")]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💾Stream", url=page_link), InlineKeyboardButton("Download 📦", url=stream_link)],
+            [InlineKeyboardButton("Delete Link 🚮", callback_data=f"msgdelconf2_{log_msg.message_id}_{get_media_file_unique_id(log_msg)}")]])
 
     return reply_markup, Stream_Text, stream_link
