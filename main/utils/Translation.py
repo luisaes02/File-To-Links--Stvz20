48e1841ef44b9a0ab5bbe97dc6eb1da94bdfed94
The_Wizard ඞ <103869920+Th3-Wizard@users.noreply.github.com>
2022-06-29 19:59:55 -0400

    Update Translation.py

--- main/utils/Translation.py
+++ main/utils/Translation.py
@@ -9,23 +9,25 @@ class Language(object):
 **Hola... Envíame cualquier archivo para obtener el enlace de descarga directa{}**\n
 <i>Que disfrutes del bot creado con ❤️❤️ por @EL_Wizard</i>\n
 <b><u>Comandos</u></b>\n
-<b>➽ /help para obtener ayuda del bot</b>"""
+<b>➽ /help para obtener ayuda del bot</b>\n
+<b>➽ /about para obtener información del bot</b>"""
 
         HELP_TEXT = """🔰 **Como Usar el bot?**
 
 <b>➽ Envia o reenvia cualquier archivo de Telegram y te dare un enlace de descarga directa</b>\n
 <b>➽ Si envia un archivo multimedia recibirá 2 enlaces uno para descargar y otro para ver</b>
+<b>➽ La velocidad de descarga es superior a <u>6mb/s</u></b>
 
-**Enlace de descarga con la velocidad más rápida 🤫🍧**
+**<b><u>La duración del enlace son 24 horas**</b></u>
 
-<b><i><u>Warning 🚸</u></i></b>
+<b><u>Regla 🚸</u></b>
 <b>La unica regla que voy a poner es que no me escriban por Privado para pedirme el código.</b></b>\n
 Alguna duda o Bug contáctame</i> <b>: <a href='https://t.me/EL_Wizard'>[ Contactar ]</a></b>"""
 
         ABOUT_TEXT = """
 <b>⚜ My Name: ➳ᴹᴿ᭄ԲɿՆ૯੮૦ՆɿՈқ✤</b>\n
 <b>⚜ Username: @file_to_link_Wizard_bot</b>\n
-<b>🔸Version: 2.0</b>\n
+<b>🔸Version: 3.0</b>\n
 <b>🔹Última Actualización: [ 29-Jun-22 ]</b>
 """
 
